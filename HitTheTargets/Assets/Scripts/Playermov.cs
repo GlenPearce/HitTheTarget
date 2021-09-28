e447a3ea449f8f83452c3ae8ee1194a16306ab44
Toastji <58075841+Toastji@users.noreply.github.com>
2021-09-28 15:23:33 +0100

    Character Controller/ Block out
    character controller is done, only needs to be changed to work with controllers, there is also a mock up blockout level for testing

--- HitTheTargets/Assets/Scripts/Playermov.cs
+++ HitTheTargets/Assets/Scripts/Playermov.cs
@@ -13,10 +13,14 @@ public class Playermov : MonoBehaviour
     public float speed;
     public float maxSpeed;
     public float dashSpeed;
+    public float dashCooldown;
     public float jumpForce;
     public float doubleJumpHeight;
     public float doubleJumpFpower;
     public float airMoveMultiply;
+    public float maxHover;
+    public float hoverPower;
+    public float airMaxSpeed;
     [Header("Player slide settings")]
     public float slideFricton;
     [Header("Player settings")]
@@ -24,7 +28,9 @@ public class Playermov : MonoBehaviour
     [Header("Camera settings")]
     public float camLerpSpeed;
 
-
+    /// <summary>
+    /// settings that are used for the various mechanics
+    /// </summary>
     Rigidbody rb;
     Collider playerColl;
 
@@ -33,34 +39,46 @@ public class Playermov : MonoBehaviour
     Vector2 camRotation;
     bool  grounded, sliding, dashing;
     bool doubleJump = false;
-
     float horizontal, vertical, mouseVertical, mouseHorizontal, dashTimer, inAirSpeed, interpolation;
+    float hoverAmount,tempMaxSpeed;
 
     void Start()
     {
         //init the variables here
         rb = GetComponent<Rigidbody>();
-        playerColl = GetComponent<Collider>(); 
+        playerColl = GetComponent<Collider>();
+        tempMaxSpeed = maxSpeed;
     }
     //-----------------------------------------------------------------------------------
+    /// <summary>
+    /// most of the stuff in update needs cleaning, via being made into methods and adding controller support aswell as organising the heirarchy and order of things are done.
+    /// </summary>
     void Update()
     {
         ///camera movement at top of update----------------------------------------------------------------------------------------
         SmoothCamera();
         cameraMov();
+
         ///Grounded Check and physics changes----------------------------------------------------------------------------------------
-        grounded = (Physics.Raycast(player.transform.position, Vector3.down, 1.3f, LayerMask.NameToLayer("ground")));
-        if (grounded&& !sliding)
+
+        grounded = Physics.Raycast(player.transform.position, Vector3.down, 1.1f);
+
+        ///stats on ground and ground resets
+        if (grounded&&!sliding)
         {
             inAirSpeed = 1;
             playerColl.material.dynamicFriction = 1;
             playerColl.material.staticFriction = 1;
+            hoverAmount = 0;
+            maxSpeed = tempMaxSpeed;
         }
+        /// air movement stats
         else
         {
             inAirSpeed = airMoveMultiply;
-            playerColl.material.dynamicFriction = slideFricton;
-            playerColl.material.staticFriction = slideFricton;
+            playerColl.material.dynamicFriction = 0;
+            playerColl.material.staticFriction = 0;
+            maxSpeed = airMaxSpeed;
         }
 
         ///take inputs for ground movement---------------------------------------------------------------------------------------------
@@ -71,7 +89,9 @@ public class Playermov : MonoBehaviour
 
         ///dash--------------------------------------------------------------------------------------------------------------------
         dashTimer += Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.LeftShift)&& dashTimer >= 2f){
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)&& dashTimer >= dashCooldown)
+        {
             if (grounded)
             {
                 dashing = true;
@@ -92,23 +112,28 @@ public class Playermov : MonoBehaviour
             dashing = false;
         }
 
-
-        ///jump--------------------------------------------------------------------------------------------------------------------
+        ///Jump--------------------------------------------------------------------------------------------------------------------
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log(grounded);
             if (grounded == true)
             {
                 jump();
                 doubleJump = true;
             }
-            if (grounded == false && doubleJump == true)
+            else if (grounded == false && doubleJump == true)
             {
                 DoubleJump();
                 doubleJump = false;
             }
         }
-        ///Slide-------------------------------------------------------------------------------------------------------------------
+
+        if (Input.GetKey(KeyCode.Space) && hoverAmount<maxHover)
+        {
+            rb.AddForce(0, hoverPower, 0);
+            Debug.Log("charging" + hoverAmount);
+            hoverAmount += Time.deltaTime;
+        }
+        //Slide-------------------------------------------------------------------------------------------------------------------
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             sliding = true;
@@ -125,7 +150,7 @@ public class Playermov : MonoBehaviour
             playerColl.material.staticFriction = 1;
         }
 
-        /// enable and disable mouse-----------------------------------------------------------------------------------------------
+        // enable and disable mouse-----------------------------------------------------------------------------------------------
         if (Input.GetKey(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
@@ -136,19 +161,20 @@ public class Playermov : MonoBehaviour
         }
     }
 
-    //-----------------------------------------------------------------------------------
-    ///fixed update for consistancy of movement feel
+    //-----------------------------------------------------------------------------------------------------------------------------
     void FixedUpdate()
     {
         grounded = (Physics.Raycast(player.transform.position, Vector3.down, 1.1f, LayerMask.NameToLayer("ground")));
-        if (grounded&&!sliding && !dashing)
+        if (!sliding && !dashing)
         {
             movement();
         }
 
     }
-    //-----------------------------------------------------------------------------------
-
+    //-----------------------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// rotates the camera based on mouse movement, multiplying the input by fixed delta time because its supposed to reduce the stuttering but not really noticed any effect
+    /// </summary>
     void cameraMov()
     {
         //grab mouse inputs and clamp the y
@@ -162,7 +188,10 @@ public class Playermov : MonoBehaviour
         camera.transform.eulerAngles = (Vector2)camRotation;
         transform.eulerAngles = new Vector2(0, mouseHorizontal);
     }
-    //-------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// grabs the axis values of wasd and converts into a vector 3
+    /// </summary>
     void inputs()
     {
         horizontal = Input.GetAxis("Horizontal");
@@ -170,8 +199,10 @@ public class Playermov : MonoBehaviour
         movementVec = new Vector3(horizontal, 0.0f, vertical);
     }
 
-    //----------------------------------------------------------------------------
-
+    //-----------------------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// basic add force in the direction of the inputs from the input method on update, there is a speed cap that ignores the y axis as not to disturb the way jumps work
+    /// </summary>
     void movement()
     {
          rb.AddRelativeForce(movementVec * speed*inAirSpeed);
@@ -179,7 +210,7 @@ public class Playermov : MonoBehaviour
          //speed limit ignores limits on Y axis
          float tempY = rb.velocity.y;
          
-        if (rb.velocity.magnitude > maxSpeed&&grounded)
+        if (rb.velocity.magnitude > maxSpeed)
         {
             rbVelocity = rb.velocity.normalized * maxSpeed;
             rb.velocity = new Vector3(rbVelocity.x, tempY, rbVelocity.z);
@@ -187,18 +218,28 @@ public class Playermov : MonoBehaviour
          
     }
 
-    //---------------------------------------------------------------------------
-
+    //----------------------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Jumps just adding impulse force to the character, the hover amount is reset upon activating the double jump.
+    /// also has the option to add frontal force to the character in the direction of the character but this didnt feel as good as it could.
+    /// </summary>
     void jump()
     {
         rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
     }
     void DoubleJump()
     {
+        //reset the y velocity so that the double jump is applied evenly even if the player is already falling.
+        rbVelocity = rb.velocity.normalized * maxSpeed;
+        rb.velocity = new Vector3(rbVelocity.x, 0, rbVelocity.z);
+        hoverAmount = 0;
         rb.AddForce(0, doubleJumpHeight, 0, ForceMode.Impulse);
         rb.AddForce(camera.transform.forward * doubleJumpFpower, ForceMode.Impulse);
     }
-    //-----------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// lerping the camera to give a smooth more "human" like movement to the camera
+    /// </summary>
     void SmoothCamera()
     {
         interpolation = camLerpSpeed * Time.deltaTime;
