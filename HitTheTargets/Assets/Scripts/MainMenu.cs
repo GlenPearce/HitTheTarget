a49730e38c66004a9893e2b9ee30f39faf4f5490
318664 <318664@gllm.ac.uk>
2021-12-17 13:19:33 +0000

    Added the Menu and changed a bit of the script
    and its all gooodddd!

--- HitTheTargets/Assets/Scripts/MainMenu.cs
+++ HitTheTargets/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject MainUI;
+    public GameObject PlayOptions;
+    //public GameObject PauseUI;
+
     // Start is called before the first frame update
-    void Start()
+    void Play()
     {
-        
+        MainUI.SetActive(false);
+        PlayOptions.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Back()
     {
-        
+        MainUI.SetActive(true);
+        PlayOptions.SetActive(false);
+    }
+
+    void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void PlayLvl1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void PlayLvl2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    void PlayLvl3()
+    {
+        SceneManager.LoadScene(3);
     }
 }
